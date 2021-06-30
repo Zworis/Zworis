7ac0aae99f218ec3183bc3ad33fad26ed2b9d6aa
Zworis <86747548+Zworis@users.noreply.github.com>
2021-06-30 23:34:45 +0200

    Add files via upload
    Hier zijn mijn 2 opdrachten voor C# voor unity: Boodschappen (Boodschappen.cs) en het telefoon programma (program.cs). Misschien zal ik ergens in de zomervakantie de andere opdrachten maken als ik me verveel

---
+++ Boodschappen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace Boodschappen_Oefen
+{
+    class Program
+    {
+        public static double Boodschap_totaal { get; private set; }
+        static void Main(string[] args)
+        {
+            //de boodschappen aan de klant uitleggen
+            Console.WriteLine("We hebben 10 Boodschappen waar je uit kan kiezen.");
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.WriteLine("We hebben:");
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(0.75));
+            Console.WriteLine("1 Hagelslag ($1,50)");
+            Console.WriteLine("2 Kaas ($2,50)");
+            Console.WriteLine("3 Patat ($1,00)");
+            Console.WriteLine("4 Brood ($2,75)");
+            Console.WriteLine("5 Appel ($0.25)");
+            Console.WriteLine("6 Jonko ($10,00)");
+            Console.WriteLine("7 Avocado ($1,09)");
+            Console.WriteLine("8 kant en klare lasagne ($4,00)");
+            Console.WriteLine("9 perensap ($1,50)");
+            Console.WriteLine("10 kippenpoten ($1,00)");
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+            //Lijst van de prijzen van de boodschappen
+            double[] Keuzes = { 1.50, 2.50, 1.00, 2.75, 0.25, 10.00, 1.09, 4.00, 1.50, 1.00 };
+            //De array maken van wat de gebruiker wilt
+            double[] Boodschap_prijs = { 0.0, 0.0, 0.0 };
+            double[] Boodschap_hoeveelheid = { 0.0, 0.0, 0.0 };
+            string[] intros = { "Wat is je eerste keuze?", "Wat is je tweede keuze?", "Wat is je laatste keuze?" };
+            double rente = 21;
+            int i = 0;
+
+            void BTW(double Btw_percentage, double hoeveelheid_product, double kosten_product, int u)
+            {
+                double kosten = hoeveelheid_product * kosten_product;
+                double eindkosten = kosten / 100 * Btw_percentage + kosten;
+                Boodschap_prijs[u] = eindkosten;
+
+            }
+
+
+
+            void kopen()
+            {
+                
+                Console.WriteLine(intros[i]);
+                Boodschap_prijs[i] = Keuzes[Convert.ToInt32(Console.ReadLine()) - 1];
+                Console.Write("Hoeveel van dit item wil je?");
+                Boodschap_hoeveelheid[i] = Convert.ToInt32(Console.ReadLine());
+                BTW(rente, Boodschap_hoeveelheid[i], Boodschap_prijs[i], i);
+                double Boodschap_totaal = Boodschap_prijs[1] + Boodschap_prijs[2] + Boodschap_prijs[0];
+                if (i == 2) 
+                {
+                    Console.WriteLine(Boodschap_totaal);
+                }
+            }
+            do
+            {
+                kopen();
+                i++;
+            } while (i < 3);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Phone_Project
+{
+    public class Phone
+    {
+        public string ceo;
+        public string merk;
+        public string versie;
+        public string os;
+        public float prijs;
+        public float verzend;
+        public Phone(string nCeo, string nMerk, string nVersie, string nOs, float nPrijs, float nVerzend)
+        {
+            ceo = nCeo;
+            merk = nMerk;
+            versie = nVersie;
+            os = nOs;
+            prijs = nPrijs;
+            verzend = nVerzend;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Alle Phones aanduiden
+            Phone Phone1 = new Phone("Steve Jobs", "Apple", "Iphone XS", "IOS", 845, 16);
+            Phone Phone2 = new Phone("Lee Byung-chull", "Samsung", "Galaxy S7", "Android", 370, 23);
+            Phone Phone3 = new Phone("Sundar Pichai", "Google", "Pixel 3", "Chrome Os", 240, 20);
+            Phone Keuze1 = new Phone("", "", "", "", 0, 0);
+
+            //De CEO's van de merken aanduiden
+            Console.WriteLine(Phone1.ceo);
+            Console.WriteLine(Phone2.ceo);
+            Console.WriteLine(Phone3.ceo);
+
+            //vragen welke mobieltjes de gebruiker wilt kopen
+            Console.WriteLine("Welke mobieltjes wilt u kopen?: " + Phone1.versie +"("+ Phone1.prijs + ")" + ", " + Phone2.versie + "(" + Phone2.prijs + ")" + ", " + Phone3.versie + "(" + Phone3.prijs + ")");
+            Console.WriteLine(Phone1.versie + " is [1]. " + Phone2.versie + " is [2]. " + Phone3.versie + " is [3]. ");
+            string keuze = Console.ReadLine();
+            if (keuze == "1")
+            {
+                Keuze1 = (Phone) Phone1.Clone();
+            }
+            else if (keuze == "2")
+            {
+                Keuze1 = (Phone)Phone2.Clone();
+            }
+            else if (keuze == "3")
+            {
+                Keuze1 = (Phone)Phone3.Clone();
+            }
+
+            Console.WriteLine("Hoeveel wilt u?");
+            int aantal = Convert.ToInt32(Console.ReadLine());
+            float lol = Keuze1.prijs;
+            double prijs = (lol * aantal) * 1.21f + Keuze1.verzend;
+
+            if (prijs > 3000)
+            {
+                prijs = prijs * 0.90f;
+            }
+            prijs = Math.Round(prijs, 2);
+            Console.WriteLine(prijs);
+            
+
+        }
+    }
+}
